c77e6d1954ef6aac37a1fdb3b66e377bc5c3fc9d
Caio Henrique Segawa Tonetti <caio.tonetti@gmail.com>
2019-04-12 12:58:23 -0300

    HOHOHO

--- generate.sh
+++ generate.sh
@@ -8,3 +8,4 @@ SEED_PATH="$TEST_PATH/seed"
 
 make
 ./generator.out -f $SEED_PATH/$1.seed < $GEN_PATH/$1.gen 1> $IN_PATH/$1.in 2> $OUT_PATH/$1.out
+ipython calcLimit.py $IN_PATH/$1.in >> $OUT_PATH/$1.out
